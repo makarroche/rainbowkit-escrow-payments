6f2df0b42ac994e87a0170fc426fc1733582b540
makarroche <macarena@mynft.com>
2023-11-30 16:57:10 -0300

    Update escrow.sol

--- contract/escrow.sol
+++ contract/escrow.sol
@@ -4,12 +4,15 @@ pragma solidity >=0.4.25 <0.9.0;
 contract escrow {
 
     address payable public player;
+    address public game;
     uint prize;
 
     constructor(){
         prize = 5;
+        game = msg.sender;
     }
 
+    //Check balance of contract
     function getBalance () public view returns (uint) {
         return address(this).balance;
     }
@@ -18,9 +21,10 @@ contract escrow {
         player = _player;
     }
 
+    //Escrow Child can only call this function
     function givePrize () external {
+        require(game == msg.sender, "Only the game can give the price");
         player.transfer(prize);
     }
 
-    
 }
