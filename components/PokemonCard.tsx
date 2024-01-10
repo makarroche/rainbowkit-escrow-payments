efb7d46f1b1241bb7e1ae64a0d42b74b08b7536f
makarroche <macarena@mynft.com>
2024-01-10 08:48:20 -0300

    Update PokemonCard.tsx

--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -1,18 +1,33 @@
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Row } from "react-bootstrap";
 import Choices from "../components/Choices";
 import { getPokemon, getPokemonDetails } from "../api/calls";
 import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
+import { useContractWrite } from "wagmi";
+import { contractEscrowAddress } from "../contract/address";
 
 type PokemonCard = {
   name?: string;
   image?: string;
 }
 
+type name = {
+  id: number
+  name: string 
+}
+
 const PokemonCard = () => {
-  const [choiceNames, setChoiceNames] = useState([]);
+  const contractABI = require("../contract/abi.json");
+  const [choiceNames, setChoiceNames] = useState<name[]>();
   const { data: corePokemon, error, isLoading } = useQuery("postsData", getPokemon);
   const [pokemon, setPokemon] = useState<PokemonCard>();
+  const [choice, setChoice] = useState(false);
+
+  const { data, isError, isSuccess, write } = useContractWrite({
+    address: contractEscrowAddress,
+    abi: contractABI,
+    functionName: "givePrize",
+  });
 
   useEffect(() => {
     if(corePokemon)
@@ -20,10 +35,14 @@ const PokemonCard = () => {
   }, [corePokemon]);
 
   useEffect(() => {
-    if(choiceNames.length>0)
+    if(choiceNames && choiceNames.length>0)
       getWinnerPokemon();
   }, [choiceNames]);
 
+  useEffect(() => {
+    if(choice) giveTokensToWinner();
+    debugger
+  }, [choice]);
 
   const getRandomNumber = ( min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -47,22 +66,34 @@ const PokemonCard = () => {
   const getWinnerPokemon = async () => {
     const randomId = getRandomNumber(0,3);
     const winner = choiceNames[randomId];
-    const image = await getPokemonDetails(winner.id)
-    setPokemon ({...pokemon, image: image });
-    setPokemon({...pokemon, name: winner.name});
+    const image = await getPokemonDetails(winner.id+1)
+    setPokemon ({ ...pokemon,name: winner.name, image: image });
+  }
+
+  const handlePlayAgain = () => {
+    setChoice(false);
+    getChoicesNames();
+    setPokemon(undefined);
+  }
+
+  const giveTokensToWinner = () => {
+    write();
   }
 
   return(
     <>
     <Card className="cardImg border-0" style={{ width: '28rem'}}>
+      <div id="card-container">
       <Card.Img variant="top" src="/images/WhatIsThisPokemon.png"/>
+      <img height = "200px" id ={ !choice ? "pokemon-shadow" : "pokemon-show"} src={pokemon?.image}/>
+      </div>
       <Card.Body className="card-body-ad ">
-        <Card.Title className="text-center mt-2 mb-4">Who's that Pokemon?</Card.Title>
-        <Card.Text>This pokemon loves bla bla bla</Card.Text>
-        <Choices names={choiceNames} pokemon={pokemon as string}></Choices>
+        <Card.Title className="text-center mt-2 mb-4">{!choice ? "Who's that Pokemon?" : `It's ${pokemon?.name} !`}</Card.Title>
+        {!choice ? <Choices names={choiceNames} pokemon={pokemon?.name} setChoice={setChoice}></Choices> : 
+        <Row><Button onClick={handlePlayAgain}>Play Again</Button></Row>}
       </Card.Body>
     </Card>
-    </>)
+</>)
     ;
 }
 
