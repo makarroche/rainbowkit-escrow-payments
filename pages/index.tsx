2be6ba792456d24f057629df9d04573641960f7e
makarroche <macarena@mynft.com>
2024-01-11 17:58:22 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,37 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import type { NextPage } from "next";
+import { useAccount, useContractWrite } from "wagmi";
 import Head from "next/head";
 import { contractEscrowAddress } from "../contract/address";
 import { Col, Container, Row } from "react-bootstrap";
-import Header from "../components/header";
+import Header from "../components/Header";
 import PokemonCard from "../components/PokemonCard";
 import Footer from "../components/Footer";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  const contractABI = require("../contract/abi.json");
+
+  const { address, isConnected } = useAccount();
+
+  const { data, isError, isLoading, isSuccess, write } = useContractWrite({
+    address: contractEscrowAddress,
+    abi: contractABI,
+    functionName: "startGame",
+    args: [address],
+  });
+
+  useEffect(() => {
+    if(isConnected) {
+    write();
+    console.log(data);
+    debugger
+    }
+  }, [isConnected, data]);
+
+
+
+
   return (
     <>
       <Container fluid className="container-color">
